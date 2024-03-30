ee002d39f578002fe114eb143c44b87984693c41
Yashbabani0 <yashbabani09@gmail.com>
2024-03-30 23:14:29 +0530

    02 complete

--- 5 js effect and features/02 insta love btn/script.js
+++ 5 js effect and features/02 insta love btn/script.js
@@ -1,1 +1,9 @@
-let 
+let coontainer = document.querySelector("#container");
+let icon = document.querySelector("i");
+
+coontainer.addEventListener("dblclick", function () {
+  icon.style.transform = "translate(-50%, -50%) scale(1)";
+  setTimeout(function () {
+    icon.style.transform = "translate(-50%, -50%) scale(0)";
+  }, 700);
+});
