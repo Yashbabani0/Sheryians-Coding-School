c488d80161ebe361b471fc833e4bbd5d1e3e72bf
Yashbabani0 <yashbabani09@gmail.com>
2024-03-31 23:20:31 +0530

    04 complete

--- 5 js effect and features/04 multpule image hovering animation/script.js
+++ 5 js effect and features/04 multpule image hovering animation/script.js
@@ -1,1 +1,15 @@
-let 
+let elem = document.querySelectorAll(".elem");
+let elemImage = document.querySelectorAll(".elem img");
+
+elem.forEach(function (elem, index) {
+  elem.addEventListener("mousemove", function (cord) {
+    elemImage[index].style.left = cord.x + "px";
+    // elemImage[index].style.top = cord.y + "px";
+  });
+  elem.addEventListener("mouseenter", function () {
+    elemImage[index].style.opacity = 1;
+  });
+  elem.addEventListener("mouseleave", function () {
+    elemImage[index].style.opacity = 0;
+  });
+});
