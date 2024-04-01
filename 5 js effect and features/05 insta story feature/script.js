d4775c1acd03154c7ac2676c4078ede4e08a2299
Yashbabani0 <yashbabani09@gmail.com>
2024-04-01 21:45:10 +0530

    update

--- 5 js effect and features/05 insta story feature/script.js
+++ 5 js effect and features/05 insta story feature/script.js
@@ -1,1 +1,29 @@
-const
+const array = [
+  {
+    dp: "https://plus.unsplash.com/premium_photo-1664267832256-176e55ccafd0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZWwlMjBnaXJsfGVufDB8fDB8fHww",
+    story:
+      "https://images.unsplash.com/photo-1611601679655-7c8bc197f0c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZWwlMjBnaXJsfGVufDB8fDB8fHww",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1611042553365-9b101441c135?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9kZWwlMjBnaXJsfGVufDB8fDB8fHww",
+    story:
+      "https://plus.unsplash.com/premium_photo-1670282393309-70fd7f8eb1ef?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW9kZWwlMjBnaXJsfGVufDB8fDB8fHww",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1617922001439-4a2e6562f328?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW9kZWwlMjBnaXJsfGVufDB8fDB8fHww",
+    story:
+      "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bW9kZWwlMjBnaXJsfGVufDB8fDB8fHww",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1617019114583-affb34d1b3cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bW9kZWwlMjBnaXJsfGVufDB8fDB8fHww",
+  },
+];
+
+let clutter = "";
+array.forEach((element, idx) => {
+  clutter += `<div class="story"><img id='${idx}' src="${element.dp}" alt="" /></div>`;
+});
+
+document.querySelector("#stories").innerHTML = clutter;
