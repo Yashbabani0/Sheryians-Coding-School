1e75f5ef843f1f1b962d6e61df579e6db0e64d07
Yashbabani0 <yashbabani09@gmail.com>
2024-04-04 05:33:05 +0530

    update

--- react-learing/src/components/Foreground.jsx
+++ react-learing/src/components/Foreground.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Card from "./Card";
 
 function Foreground() {
+  const ref = useRef(null);
+  const data = [
+    {
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "blue",
+      },
+    },
+  ];
+  useState();
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
-      <Card />
+    <div
+      ref={ref}
+      className="fixed top-0 left-0 z-[3] w-full h-full gap-10 flex flex-wrap p-5"
+    >
+      {data.map((item, index) => (
+        <Card data={item} reference={ref} />
+      ))}
     </div>
   );
 }
