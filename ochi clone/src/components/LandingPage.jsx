81a428e7b5629e7287e6929ebde2aee2e4ed39db
Yashbabani0 <yashbabani09@gmail.com>
2024-04-06 12:17:53 +0530

    update

---
+++ ochi clone/src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div className="w-full h-screen bg-zinc-900">
+      <div className="textstructure">
+        <div className="masker element-founders">
+          <h1>We create eye-opening presentations</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
